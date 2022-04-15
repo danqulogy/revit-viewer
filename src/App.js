d22c82b59f01ad3275986010e53fd47796f4e678
Ben <danquahwhite@gmail.com>
2022-04-15 14:52:41 +0000

    handled model error

--- src/App.js
+++ src/App.js
@@ -1,5 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+
+class AppMain extends Component{
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      view: null
+    }
+  }
+
+  handleViewerError(error){
+    console.error("Error loading viewer")
+  }
+
+  /**
+   * After the viewer loads a document, we need to select which viewable
+   * to display in our component
+   * @param {any} doc 
+   * @param {Array} viewables 
+   */
+  handleDocumentLoaded(doc, viewables){
+    if(viewables.length === 0){
+      console.error('Document contains no viewables.')
+    }
+    else{
+      // Select the first viewable in the list to use in our viewer component
+      this.setState({view: viewables[0]})
+    }
+  }
+
+  handleDocumentError(viewer, error){
+    console.log('Error loading a document')
+  }
+
+}
+
+
 
 function App() {
   return (
